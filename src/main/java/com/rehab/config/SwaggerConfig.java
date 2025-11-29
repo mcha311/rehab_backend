ffbb8ce22acd97afda602b65aa8e074c8ebf6e8c
Kim-Dongdong <kdssos11@naver.com>
2025-11-29 12:41:34 +0900

    [FEAT] SwaggerConfig 추가

---
+++ src/main/java/com/rehab/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.rehab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI RehabAPI() {
+		Info info = new Info()
+			.title("Rehab server API")
+			.description("Rehab server API 명세서")
+			.version("1.0.0");
+
+		String jwtSchemeName = "JWT TOKEN";
+		// API 요청헤더에 인증정보 포함
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+		// SecuritySchemes 등록
+		Components components = new Components()
+			.addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+				.name(jwtSchemeName)
+				.type(SecurityScheme.Type.HTTP) // HTTP 방식
+				.scheme("bearer")
+				.bearerFormat("JWT"));
+
+		return new OpenAPI()
+			.addServersItem(new Server().url("/"))
+			.info(info)
+			.addSecurityItem(securityRequirement)
+			.components(components);
+	}
+}
