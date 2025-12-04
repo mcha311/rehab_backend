6d55eac2a76caaa4158db4a56c588a1e0ba4504e
Kim-Dongdong <kdssos11@naver.com>
2025-12-04 20:40:55 +0900

    [REFACTOR] "api/**" 보안 제외

--- src/main/java/com/rehab/config/SecurityConfig.java
+++ src/main/java/com/rehab/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig {
 					"/oauth2/**",
 					"/swagger-ui/**",
 					"/swagger-ui.html",
-					"/v3/api-docs/**"
+					"/v3/api-docs/**",
+					"/api/**"
 				).permitAll()
 				.anyRequest().authenticated()
 			)
