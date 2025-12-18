138fa319a58c4ef90587c901f8c9062e2bb6bc2b
chahyeonmin91 <chahyeonmin91@gmail.com>
2025-12-19 02:40:38 +0900

    //

--- src/main/java/com/rehab/service/userService/UserService.java
+++ src/main/java/com/rehab/service/userService/UserService.java
@@ -4,7 +4,7 @@ import com.rehab.domain.entity.User;
 import com.rehab.dto.user.UserProfileDto;
 
 public interface UserService {
-
+//
 	UserProfileDto.ProfileResponse getMyProfile(User user);
 
 	UserProfileDto.ProfileResponse updateMyProfile(User user, UserProfileDto.ProfileUpdateRequest request);
